f10dc5811ff8a9b09903b1793db2f4364d4c602c
akorol <andriykorol@yahoo.com>
2020-03-16 18:16:13 +0200

    send update

--- src/app/shared/services/emailSender/email-sender.service.ts
+++ src/app/shared/services/emailSender/email-sender.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 const httpOptions = {
-  headers: new HttpHeaders({
+  headers: new HttpHeaders({    
     'Content-Type':  'application/json',
     'Authorization': 'bearer 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'
   })
@@ -16,20 +16,24 @@ const httpOptions = {
 export class EmailSenderService {
 
 
-  readonly ROOT_URL = 'https://api.sendgrid.com/v3/mail/send'
+  readonly ROOT_URL = 'https://cors-anywhere.herokuapp.com/https://api.sendgrid.com/v3/mail/send';
+  CORS_URL = '';
 
   constructor(private Http: HttpClient) {}
 
 
   sendMessage(body: any) {
 
-    var subject = new Subject<number>()
-    const req = new HttpRequest('POST', this.ROOT_URL, body, {
-        reportProgress: true,
-    });
-    console.log('active send service', req);
+    var subject = new Subject<number>();
+    
+    return this.Http.post(this.ROOT_URL, body, httpOptions);
+
+    // const req = new HttpRequest('POST', this.ROOT_URL, body, {
+    //     reportProgress: true,
+    // });
+    // console.log('active send service', req);
     // return this.Http.post(this.ROOT_URL, body, httpOptions);
-    return this.Http.request(req);
+    // return this.Http.request(req);
   }
 
 }
