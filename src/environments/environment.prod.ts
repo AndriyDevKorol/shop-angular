f8a861f1042dec041ee4257b3aaeb9a15d9dfbc2
Dev@K <andriykorol@yahoo.com>
2020-03-17 19:40:14 +0200

    upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  config: {
+    apiKey: "AIzaSyA-w6ueo9NY4uyB7UB_fu12qS0gwqHg0hk",
+    authDomain: "web-shop-fc3e0.firebaseapp.com",
+    databaseURL: "https://web-shop-fc3e0.firebaseio.com",
+    projectId: "web-shop-fc3e0",
+    storageBucket: "web-shop-fc3e0.appspot.com",
+    messagingSenderId: "445703680065",
+    appId: "1:445703680065:web:f33d33d116e9cdfb7b96c0",
+    measurementId: "G-T2WJDJGMJ7",
+    SENDGRID_KEY: "bearer ",
+    SENGRID_URL: 'https://cors-anywhere.herokuapp.com/https://api.sendgrid.com/v3/mail/send',
+  }
+};
