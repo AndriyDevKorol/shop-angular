c3defb851e5732a7787b24d5937506e6dabc71d6
Andriy Korol <andriykorol@yahoo.com>
2020-05-18 21:52:15 +0300

    small update

--- src/app/components/home/home-content/home-content.component.ts
+++ src/app/components/home/home-content/home-content.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/modules/Product';
+import { ProductService } from 'src/app/shared/services/product/product.service';
+import { ProductFilterService } from 'src/app/shared/services/filter/product-filter.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home-content',
@@ -9,9 +13,42 @@ import { Product } from 'src/app/shared/modules/Product';
 export class HomeContentComponent implements OnInit {
   product: Product;
   products: Product[];
-  constructor() { }
+  categories: any;
+  searchFormControl: FormControl;
+  editProductKey: string;
+  isAdmin = this.afauth.auth.currentUser;
+  selectedCategory: string;
+  categoryList: string[];
+  searchCategory;
+  searchTitle;
+  searchPrice;
+  searchBody;
+
+  constructor(
+    public productService: ProductService,
+    public filterService: ProductFilterService,
+    private afauth: AngularFireAuth
+    ) { }
 
   ngOnInit() {
+    this.filterService.selectedCategoryEvent.subscribe(res =>  {this.getProductList(res)});
+    // this.loadData();
+  }
+
+  getProductList(selectedCategory: string) {
+    this.productService
+    .getProducts().snapshotChanges()
+    .subscribe(data => {
+      let productsList = data.map(e => {
+        return {
+          $key: e.key,
+          ...e.payload.val()
+        }
+      }).reverse();
+      this.products = this.filterService.getCategory(productsList, selectedCategory);
+      this.categoryList = [...new Set(productsList.map(res => res.category))];
+      this.filterService.getCategoryListListener(this.categoryList);
+    });
   }
 
 }
