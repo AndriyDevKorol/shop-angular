cb6e8aee7ddce4fc9f8400ecdc946e2decc19931
Andriy Korol <andriykorol@yahoo.com>
2020-11-21 23:10:27 +0200

    url

--- src/app/core/components/product/product.component.ts
+++ src/app/core/components/product/product.component.ts
@@ -6,6 +6,8 @@ import { takeUntil } from 'rxjs/operators';
 import { ProductService } from '../../services/product.service';
 import { ProductModel } from '../../../models/product.model';
 import { ShareDataService } from '../../services/shareData.service';
+import { ProductsModule } from '../products.module';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 
 @Component({
@@ -22,16 +24,21 @@ export class ProductComponent implements OnInit, OnDestroy {
   isAdmin = this.afauth.auth.currentUser;
   countVal = 1;
   editProductKey: string;
+  productId: string;
+
+  private subscription: Subscription;
 
   constructor(
     private router: Router,
     private productService: ProductService,
     private route: ActivatedRoute,
     private shareDataService: ShareDataService,
-    private afauth: AngularFireAuth
+    private afauth: AngularFireAuth,
   ) { }
 
   ngOnInit() {
+    // this.productId = this.product.$key;
+    this.subscription = this.route.params.subscribe(params=>this.productId=params['id']);
   }
 
 
