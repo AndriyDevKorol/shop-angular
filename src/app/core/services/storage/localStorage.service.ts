5d0f10ae82c419f97725891c3528ebd7d19a4ee1
Andriy Korol <andriykorol@yahoo.com>
2020-12-15 16:17:56 +0200

    clear cart

--- src/app/core/services/storage/localStorage.service.ts
+++ src/app/core/services/storage/localStorage.service.ts
@@ -1,36 +1,48 @@
 import { Inject, Injectable } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 
 @Injectable()
 export class LocalStorageService  {
-     constructor(
-       @Inject(LOCAL_STORAGE) private storage: StorageService
-       ) { }
+  // private dataStorage = new BehaviorSubject<string[]>([]);
+  // dataStorageEvent = this.dataStorage.asObservable();
 
-     setDataToLocalStorage(key:string, value: any) {
-      let storageData:string[] = this.storage.get(key) || [];
 
-      if(storageData.some(item => item === value)){return}
+    constructor(
+      @Inject(LOCAL_STORAGE) private storage: StorageService
+      ) { }
 
-      this.storage.set(key, [...storageData, value]);
-     }
+    setDataToLocalStorage(key:string, value: any) {
+    let storageData:string[] = this.storage.get(key) || [];
 
-     getLocalStorageData(STORAGE_KEY: any):String[] {
-      return this.storage.get(STORAGE_KEY);
-     }
+    if(storageData.some(item => item === value)){return}
 
-     removeLocalStoargeData(key: string) {
+    storageData.push(value);
+    this.storage.set(key, storageData);
+    // console.log('service', this.dataStorage);
+    // this.dataStorage.next(storageData);
+    }
 
-      let items = this.storage
-      .get('cart')
-      .filter(el => el !== key);
+    getLocalStorageData(STORAGE_KEY: any):String[] {
+    return this.storage.get(STORAGE_KEY);
+    }
 
-      return this.setDataToLocalStorage('cart', items);
-     }
+    updateStore(data){
+      console.log('dd', data);
+      // this.dataStorage.next(data);
+    }
 
-     clearLocalStoargeData() {
-      this.storage.clear();
-     }
+    removeLocalStoargeData(key: string) {
+
+    let items = this.storage
+    .get('cart')
+    .filter(el => el !== key);
+
+    return this.setDataToLocalStorage('cart', items);
+    }
+
+    clearLocalStoargeData() {
+    this.storage.clear();
+    }
 }
