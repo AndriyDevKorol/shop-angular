90e4a9bef982820815ad709603f6717d5f55aecf
Andriy Korol <andriykorol@yahoo.com>
2021-01-03 22:15:59 +0200

    get unit category

--- src/app/client/left-section/left-menu/left-menu.component.ts
+++ src/app/client/left-section/left-menu/left-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { combineAll } from 'rxjs/operators';
+import { ProductService } from 'src/app/core/services/product.service';
 import { ShareDataService } from 'src/app/core/services/shareData.service';
 import { ProductModel } from 'src/app/models/product.model';
 
@@ -9,12 +11,22 @@ import { ProductModel } from 'src/app/models/product.model';
 })
 export class LeftMenuComponent implements OnInit {
   @Input() products$: ProductModel[];
+  categories: string[] = [];
+
 
   constructor(
     private shareDataService: ShareDataService,
+    private productService: ProductService
     ) { }
 
   ngOnInit() {
+      this.productService.getProducts().forEach(res => {
+        this.products$ = res;
+        this.products$.forEach(res => {
+          this.categories.push(res.category);
+          this.categories = [...new Set(this.categories)];
+        })
+     })
   }
 
   getValue(category: string){
